3d182cc81024117396db9875b3a494b547afcbf0
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2020-07-22 18:12:13 -0700

    start query mapping

--- app.js
+++ app.js
@@ -57,19 +57,51 @@ window.onload = function() {
         }
     })
 
-    // Query Page Map
-    function buildMap() {
 
+    function addMapBaseLayer() {
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
     
-        L.marker([51.5, -0.09]).addTo(map)
-            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-            .openPopup();
-
+        // L.marker([51.5, -0.09]).addTo(map)
+        //     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        //     .openPopup();
+    
             map.setView([0,0], 0)
             map.setZoom(1)
+    }
+
+    // Query Page Map
+    function buildMap(content, lat, lon) {
+
+            let geojsonFeature = {
+                "type": "Feature",
+                "properties": {
+                    "name": "Coors Field",
+                    "amenity": "Baseball Stadium",
+                    "popupContent": content
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [`${lon}`, `${lat}`]
+                }
+            };
+
+
+            let geojsonMarkerOptions = {
+                radius: 8,
+                fillColor: "#ff7800",
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
+            
+            L.geoJSON(geojsonFeature, {
+                pointToLayer: function (feature, latlng) {
+                    return L.circleMarker(latlng, geojsonMarkerOptions).bindPopup(feature.properties.popupContent)
+                }
+            }).addTo(map)
     }
 
     function fetchByScientificName(name) {
@@ -171,6 +203,14 @@ window.onload = function() {
             } else if (tableRadio.checked == false && mapRadio.checked == true) {
                 queryTableContainer.style.display = 'none'
                 mapContainer.style.display = 'flex'
+                dataArr.forEach(hit => {
+                    let x = hit._source
+
+                    if (year == x.yearCollected) {
+                        console.log('LAT: ' + x.decimalLatitude + ' LON: ' + x.decimalLongitude);
+                        buildMap(year, x.decimalLatitude, x.decimalLongitude)
+                    }
+                })
             } else {
                 console.log('Select map or table');
             }
@@ -288,7 +328,7 @@ window.onload = function() {
         homeNav.classList.remove('nav-btn')
         queryNav.classList.add('nav-btn')
         openPage('query-tab')
-        buildMap()
+        addMapBaseLayer()
     })
 
     /******************** 
