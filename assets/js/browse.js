df7eafff3946364999931a9953c3d58634897544
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2020-10-02 16:44:28 -0700

    misc css adjustments

--- assets/js/browse.js
+++ assets/js/browse.js
@@ -4,11 +4,10 @@ window.onload = function() {
     const projBaseURL = 'https://raw.githubusercontent.com/futres/FutresAPI/master/data/projects.json'
     const chartSelect = document.getElementById('chart-select')
 
-    /******************** 
-        BROWSE TAB 
-    *********************/
-
+   //Projects Table
    fetchProjects()
+
+   // Summary Tables
    countryTableData()
    speciesTableData()
    yearCollectedTableData()
@@ -366,51 +365,4 @@ let coll = document.getElementsByClassName("collapsible-button");
        modal.style.display = "none";
        }
    }
-
-   // SORT FUNCTION FOR TABLES (TAKEN OUT OF HTML FILE)
-   function sortTable(tableId, n) {
-       let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-
-       table = document.getElementById(tableId);
-       switching = true;
-       dir = "asc";
-       while (switching) {
-           switching = false;
-           rows = table.getElementsByTagName("TR");
-
-           for (i = 1; i < (rows.length - 1); i++) {
-               shouldSwitch = false;
-               x = rows[i].getElementsByTagName("TD")[n];
-               y = rows[i + 1].getElementsByTagName("TD")[n];
-
-               let cmpX=isNaN(parseInt(x.innerHTML))?x.innerHTML.toLowerCase():parseInt(x.innerHTML);
-               let cmpY=isNaN(parseInt(y.innerHTML))?y.innerHTML.toLowerCase():parseInt(y.innerHTML);
-               cmpX=(cmpX=='-')?0:cmpX;
-               cmpY=(cmpY=='-')?0:cmpY;
-
-               if (dir == "asc") {
-                   if (cmpX > cmpY) {
-                       shouldSwitch= true;
-                       break;
-                   }
-               } else if (dir == "desc") {
-                   if (cmpX < cmpY) {
-                       shouldSwitch= true;
-                       break;
-                   }
-               }
-           }
-           if (shouldSwitch) {
-               rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-               switching = true;
-               switchcount ++;      
-           } else {
-               if (switchcount == 0 && dir == "asc") {
-                   dir = "desc";
-                   switching = true;
-               }
-           }
-       }
-       }
-
 }
